5f069c9c0c15ab697c486a3193b9b51a7c268621
DanielJaureguiGonzales <dank_lp123@outlook.com>
2021-10-27 06:08:52 -0500

    account partial

---
+++ src/main/java/com/dreamteam/dhome/register/microservice/query/projections/AccountViewRepository.java
@@ -0,0 +1,11 @@
+package com.dreamteam.dhome.register.microservice.query.projections;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountViewRepository extends JpaRepository<AccountView,String> {
+    Optional<AccountView> getByUsername(String username);
+}
