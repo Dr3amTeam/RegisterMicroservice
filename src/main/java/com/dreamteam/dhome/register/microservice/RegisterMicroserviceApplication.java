c87925edda70eb778d4396ae11ad9529fa3746e7
DanielJaureguiGonzales <dank_lp123@outlook.com>
2021-10-26 23:41:51 -0500

    base

---
+++ src/main/java/com/dreamteam/dhome/register/microservice/RegisterMicroserviceApplication.java
@@ -0,0 +1,15 @@
+package com.dreamteam.dhome.register.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class RegisterMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/dreamteam/dhome/register/microservice/config/AxonConfig.java
@@ -0,0 +1,9 @@
+package com.dreamteam.dhome.register.microservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AxonConfig {
+
+}
